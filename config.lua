25935e44d2d5e6eb9b19564a23abb7ba1a1699fa
Kalle Chen <kallechen.tw@gmail.com>
2023-08-13 18:24:05 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,12 +9,12 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = false
+lvim.colorscheme = "tokyonight"
+lvim.format_on_save = false
 lvim.lint_on_save = true
-lvim.colorscheme = "github_dark"
 vim.opt.relativenumber = true
-lvim.transparent_window = false
+lvim.transparent_window = true
 vim.opt.cursorline = false
-
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -22,10 +22,18 @@ vim.opt.cursorline = false
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+vim.keymap.set("i", "jk", "<ESC>", {silent = true})
 lvim.keys.normal_mode["E"] = "$"
 lvim.keys.normal_mode["B"] = "^"
 lvim.keys.visual_mode["E"] = "$"
 lvim.keys.visual_mode["B"] = "^"
+
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.builtin.which_key.mappings["f"] = {
+    "<cmd>Telescope find_files<CR>", "Files"
+}
 
 lvim.builtin.which_key.mappings["r"] = {
     name = "Replace",
@@ -37,9 +45,11 @@ lvim.builtin.which_key.mappings["r"] = {
     f = {"<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer"}
 }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "Todo",
-  l = {"<cmd>TodoTrouble<CR>", "Trouble List"}
+    name = "Todo",
+    l = {"<cmd>TodoTrouble<CR>", "Trouble List"}
 }
+-- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+-- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
@@ -63,6 +73,10 @@ lvim.builtin.which_key.mappings["t"] = {
 --   },
 -- }
 
+-- Change theme settings
+-- lvim.builtin.theme.options.dim_inactive = true
+-- lvim.builtin.theme.options.style = "storm"
+
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 -- lvim.builtin.which_key.mappings["t"] = {
@@ -72,32 +86,45 @@ lvim.builtin.which_key.mappings["t"] = {
 --   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
 --   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
 --   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+--   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 -- }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
+-- lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.filters.dotfiles = false
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
     "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx",
-    "css", "rust", "java", "yaml", 'http'
+    "css", "rust", "java", "yaml"
 }
 
 lvim.builtin.treesitter.ignore_install = {"haskell"}
-lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.highlight.enable = true
 
 -- generic LSP settings
 
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+-- lvim.lsp.installer.setup.ensure_installed = {
+--     "sumneko_lua",
+--     "jsonls",
+-- }
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
+
 -- ---@usage disable automatic installation of servers
--- lvim.lsp.automatic_servers_installation = false
+-- lvim.lsp.installer.setup.automatic_installation = false
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
@@ -106,8 +133,8 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- require("lvim.lsp.manager").setup("pyright", opts)
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
--- vim.tbl_map(function(server)
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
 --   return server ~= "emmet_ls"
 -- end, lvim.lsp.automatic_configuration.skipped_servers)
 
@@ -136,38 +163,6 @@ lvim.builtin.treesitter.highlight.enabled = true
 --     filetypes = { "typescript", "typescriptreact" },
 --   },
 -- }
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-    {
-        command = "prettier",
-        filetypes = {
-            "html", "htmldjango", "javascript", "json", "javascriptreact",
-            "typescript", "typescriptreact", "css", "http", "solidity"
-        }
-    }, {command = "black", filetypes = {"python"}},
-    {command = "lua-format", filetypes = {"lua"}}
-}
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-    {command = "flake8", filetypes = {"python"}}, {
-        command = "eslint",
-        filetypes = {
-            "javascript", "javascriptreact", "typescript", "typescriptreact"
-        }
-    }, {command = "luacheck", filetypes = {"lua"}},
-    {command = "solhint", filetypes = {"solidity"}}
-}
-
-local code_actions = require "lvim.lsp.null-ls.code_actions"
-code_actions.setup {
-    {
-        command = "eslint",
-        filetypes = {
-            "javascript", "javascriptreact", "typescript", "typescriptreact"
-        }
-    }
-}
 
 -- -- set additional linters
 -- local linters = require "lvim.lsp.null-ls.linters"
@@ -187,30 +182,59 @@ code_actions.setup {
 --   },
 -- }
 
--- Additional Plugins
--- lvim.plugins = {
---     {"folke/tokyonight.nvim"},
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+    {
+        command = "prettier",
+        filetypes = {
+            "html", "htmldjango", "javascript", "json", "javascriptreact",
+            "typescript", "typescriptreact", "css", "http"
+        }
+    }, {command = "black", filetypes = {"python"}},
+    {command = "lua-format", filetypes = {"lua"}}
+}
 
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+    {command = "flake8", filetypes = {"python"}}, {
+        command = "eslint",
+        filetypes = {
+            "javascript", "javascriptreact", "typescript", "typescriptreact"
+        }
+    }, {command = "luacheck", filetypes = {"lua"}}
+}
+
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup {
+    {
+        command = "eslint",
+        filetypes = {
+            "javascript", "javascriptreact", "typescript", "typescriptreact"
+        }
+    }
+}
 
 lvim.plugins = {
+    {
+        "zbirenbaum/copilot.lua",
+        event = {"VimEnter"},
+        config = function()
+            vim.defer_fn(function()
+                require("copilot").setup {
+                    plugin_manager_path = get_runtime_dir() ..
+                        "/site/pack/packer",
+                    suggestion = {
+                        auto_trigger = true,
+                        keymap = {
+                            accept = "<C-e>",
+                            next = "<C-l>",
+                            prev = "<C-h>"
+                        }
+                    }
+                }
+            end, 100)
+        end
+    }, {"zbirenbaum/copilot-cmp", after = {"copilot.lua", "nvim-cmp"}},
     {"folke/trouble.nvim", cmd = "TroubleToggle"}, {
         "windwp/nvim-spectre",
         event = "BufRead",
@@ -338,121 +362,51 @@ lvim.plugins = {
 
             })
         end
-    }, {
-        "karb94/neoscroll.nvim",
-        event = "WinScrolled",
-        config = function()
-            require('neoscroll').setup({
-                -- All these keys will be mapped to their corresponding default scrolling animation
-                mappings = {
-                    "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt",
-                    "zz", "zb"
-                },
-                hide_cursor = true, -- Hide cursor while scrolling
-                stop_eof = true, -- Stop at <EOF> when scrolling downwards
-                use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-                respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-                cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-                easing_function = nil, -- Default easing function
-                pre_hook = nil, -- Function to run before the scrolling animation starts
-                post_hook = nil -- Function to run after the scrolling animation ends
-            })
-        end
-    }, {'projekt0n/github-nvim-theme'}, {'TovarishFin/vim-solidity'}, {
+    }, {'TovarishFin/vim-solidity'}, {
         "folke/todo-comments.nvim",
         event = "BufRead",
         config = function() require("todo-comments").setup() end
-    }, {'ellisonleao/glow.nvim'},
-    {'pwntester/octo.nvim', config = function() require"octo".setup() end},
-    {'andweeb/presence.nvim'}, {'mg979/vim-visual-multi'},
-    {"github/copilot.vim"}, {"wakatime/vim-wakatime"}, {
+    }, {'andweeb/presence.nvim'}, {'mg979/vim-visual-multi'},
+    {"wakatime/vim-wakatime"}, {
         "f-person/git-blame.nvim",
         event = "BufRead",
         config = function()
             vim.cmd "highlight default link gitblame SpecialComment"
             vim.g.gitblame_enabled = 0
         end
-    }, {
-        "tpope/vim-fugitive",
-        cmd = {
-            "G", "Git", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove",
-            "GDelete", "GBrowse", "GRemove", "GRename", "Glgrep", "Gedit"
-        },
-        ft = {"fugitive"}
     }, {"p00f/nvim-ts-rainbow"}, {"sindrets/diffview.nvim", event = "BufRead"},
     {
         "windwp/nvim-ts-autotag",
         event = "InsertEnter",
         config = function() require("nvim-ts-autotag").setup() end
-    }, {"rafamadriz/neon"}, {
-        'navarasu/onedark.nvim',
-        config = function()
-            require('onedark').setup {style = 'darker'}
-            require('onedark').load()
-        end
     }, {'ianding1/leetcode.vim'}, {
-        "lukas-reineke/indent-blankline.nvim",
-        event = "BufRead",
-        setup = function()
-            vim.g.indentLine_enabled = 1
-            vim.g.indent_blankline_char = "|"
-            vim.g.indent_blankline_filetype_exclude = {
-                "help", "terminal", "dashboard"
-            }
-            vim.g.indent_blankline_buftype_exclude = {"terminal"}
-            vim.g.indent_blankline_show_trailing_blankline_indent = false
-            vim.g.indent_blankline_show_first_indent_level = false
-            vim.g.indent_blankline_show_current_context = true
-            vim.g.indent_blankline_show_current_context_start = true
-        end
-    }, {
         "iamcco/markdown-preview.nvim",
         run = "cd app && npm install",
         ft = "markdown",
         config = function() vim.g.mkdp_auto_start = 1 end
-    }, {
-        "NTBBloodbath/rest.nvim",
-        requires = {"nvim-lua/plenary.nvim"},
-        config = function()
-            require("rest-nvim").setup({
-                -- Open request results in a horizontal split
-                result_split_horizontal = false,
-                -- Keep the http file buffer above|left when split horizontal|vertical
-                result_split_in_place = false,
-                -- Skip SSL verification, useful for unknown certificates
-                skip_ssl_verification = false,
-                -- Highlight request on run
-                highlight = {enabled = true, timeout = 150},
-                result = {
-                    -- toggle showing URL, HTTP info, headers at top the of result window
-                    show_url = true,
-                    show_http_info = true,
-                    show_headers = true
-                },
-                -- Jump to request line on run
-                jump_to_request = false,
-                env_file = '.env',
-                custom_dynamic_variables = {},
-                yank_dry_run = true
-            })
-        end
     }
-
 }
 
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_assume_mapped = true
-vim.g.copilot_tab_fallback = ""
-local cmp = require "cmp"
-lvim.builtin.cmp.mapping["<C-e>"] = function(fallback)
-    cmp.mapping.abort()
-    local copilot_keys = vim.fn["copilot#Accept"]()
-    if copilot_keys ~= "" then
-        vim.api.nvim_feedkeys(copilot_keys, "i", true)
-    else
-        fallback()
-    end
-end
+-- lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"
+-- table.insert(lvim.builtin.cmp.sources, 1, { name = "copilot" })
+-- Additional Plugins
+-- lvim.plugins = {
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+-- }
 
-vim.g.leetcode_solution_filetype = 'javascript'
-vim.g.leetcode_browser = "chrome"
+-- Autocommands (https://neovim.io/doc/user/autocmd.html)
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   pattern = { "*.json", "*.jsonc" },
+--   -- enable wrap mode for json files only
+--   command = "setlocal wrap",
+-- })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "zsh",
+--   callback = function()
+--     -- let treesitter use bash highlight for zsh files as well
+--     require("nvim-treesitter.highlight").attach(0, "bash")
+--   end,
+-- })
